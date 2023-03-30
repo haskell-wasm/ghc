4583c26bb9a0e105921355b196d7320597fc22ad
Cheng Shao <terrorjack@type.dance>
2023-03-30 18:38:43 +0000

    ci: ensure that all non-i386 pipelines do parallel xz compression
    We can safely enable parallel xz compression for non-i386 pipelines.
    However, previously we didn't export XZ_OPT, so the xz process won't
    see it if XZ_OPT hasn't already been set in the current job.
    
    (cherry picked from commit ef1548c499d69a156d9f0772c859e0e5cdb0892b)

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -517,7 +517,7 @@ function build_hadrian() {
   # hadrian calls tar/xz to produce bindist, there's no other build
   # work taking place.
   if [[ "${CI_JOB_NAME:-}" != *"i386"* ]]; then
-    XZ_OPT="${XZ_OPT:-} -T$cores"
+    export XZ_OPT="${XZ_OPT:-} -T$cores"
   fi
 
   if [[ -n "${REINSTALL_GHC:-}" ]]; then
