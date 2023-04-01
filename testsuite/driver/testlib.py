ad9838c816d68493a8081505e7fb7f8e42016ac0
Cheng Shao <terrorjack@type.dance>
2023-04-01 19:48:31 +0000

    testsuite: add the req_ghc_with_threaded_rts predicate
    This patch adds the req_ghc_with_threaded_rts predicate to the
    testsuite to assert the platform has threaded RTS, and mark some tests
    as req_ghc_with_threaded_rts. Also makes ghc_with_threaded_rts a
    config field instead of a global variable.
    
    (cherry picked from commit aa6afe8ab48b5fbe09d332a25ce6af4719c5cb44)

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -313,6 +313,11 @@ def req_target_smp( name, opts ):
 def req_process( name, opts ):
     if not config.have_process:
         opts.skip = True
+
+def req_ghc_with_threaded_rts( name, opts ):
+    if not config.ghc_with_threaded_rts:
+        opts.skip = True
+
 def ignore_stdout(name, opts):
     opts.ignore_stdout = True
 
