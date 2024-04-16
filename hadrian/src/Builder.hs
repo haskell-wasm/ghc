4f02268c93ba6b825a065438ef0c0691fe2129b0
Cheng Shao <terrorjack@type.dance>
2024-04-16 19:09:17 +0000

    hadrian: remove obsolete Patch logic
    This commit removes obsolete Patch logic from hadrian, given we no
    longer need to patch the gmp tarball when building in-tree GMP.
    
    (cherry picked from commit 65b4b92fa1e1989d055108a6077cc9119ee28acd)
    (cherry picked from commit 920655005d439bd2c6cf11031c8569c7bba7339c)

--- hadrian/src/Builder.hs
+++ hadrian/src/Builder.hs
@@ -8,10 +8,7 @@ module Builder (
     -- * Builder properties
     builderProvenance, systemBuilderPath, builderPath, isSpecified, needBuilders,
     runBuilder, runBuilderWith, runBuilderWithCmdOptions, getBuilderPath,
-    builderEnvironment,
-
-    -- * Ad hoc builder invocation
-    applyPatch
+    builderEnvironment
     ) where
 
 import Control.Exception.Extra (Partial)
@@ -178,7 +175,6 @@ data Builder = Alex
              | MergeObjects Stage -- ^ linker to be used to merge object files.
              | Nm
              | Objdump
-             | Patch
              | Python
              | Ranlib
              | Testsuite TestMode
@@ -441,7 +437,6 @@ systemBuilderPath builder = case builder of
     Makeinfo        -> fromKey "makeinfo"
     Nm              -> fromKey "nm"
     Objdump         -> fromKey "objdump"
-    Patch           -> fromKey "patch"
     Python          -> fromKey "python"
     Ranlib          -> fromKey "ranlib"
     Testsuite _     -> fromKey "python"
@@ -492,15 +487,6 @@ systemBuilderPath builder = case builder of
 -- | Was the path to a given system 'Builder' specified in configuration files?
 isSpecified :: Builder -> Action Bool
 isSpecified = fmap (not . null) . systemBuilderPath
-
--- | Apply a patch by executing the 'Patch' builder in a given directory.
-applyPatch :: FilePath -> FilePath -> Action ()
-applyPatch dir patch = do
-    let file = dir -/- patch
-    needBuilders [Patch]
-    path <- builderPath Patch
-    putBuild $ "| Apply patch " ++ file
-    quietly $ cmd' [Cwd dir, FileStdin file] [path, "-p0"]
 
 -- Note [cmd wrapper]
 -- ~~~~~~~~~~~~~~~~~~
