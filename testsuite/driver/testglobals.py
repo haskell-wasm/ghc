25a6b98e93b481681bb1f3e1ec8953f0bf23e8d6
Cheng Shao <terrorjack@type.dance>
2024-04-30 14:17:50 +0000

    testsuite: fix req_target_smp predicate
    (cherry picked from commit a580722ecb716b917a16521d5185c1dbc24d0ab9)
    (cherry picked from commit 4f317bfdc47e609fc26f1e1483a71b581ebcccbb)

--- testsuite/driver/testglobals.py
+++ testsuite/driver/testglobals.py
@@ -163,7 +163,7 @@ class TestConfig:
         self.ghc_has_smp = True
 
         # Does the target have SMP support?
-        self.target_has_smp = True
+        self.target_has_smp = False
 
         # Is gdb available?
         self.have_gdb = False
