8dff5b236a2eb5ec2036504440e343a6eda4340c
Cheng Shao <terrorjack@type.dance>
2024-05-22 14:30:03 +0000

    compiler: avoid saving foreign call target to local when there are no caller-save GlobalRegs
    This patch makes the STG->Cmm backend avoid saving foreign call target
    to local when there are no caller-save GlobalRegs.
    
    Since 321941a8ebe25192cdeece723e1058f2f47809ea, when we lower a
    foreign call, we unconditionally save the foreign call target to a
    temporary local first, then rely on cmmSink to clean it up later,
    which only happens with -fcmm-sink (implied by -O) and not in
    unoptimized code.
    
    And this is troublesome for the wasm backend NCG, which needs to infer
    a foreign call target symbol's type signature from the Cmm call site.
    Previously, the NCG has been emitting incorrect type signatures for
    unoptimized code, which happens to work with `wasm-ld` most of the
    time, but this is never future-proof against upstream toolchain
    updates, and it causes horrible breakages when LTO objects are
    included in linker input. Hence this patch.
    
    (cherry picked from commit 8dd8a076058baca45ac52ace25b9c2797d61ef84)
    (cherry picked from commit 2b2a3db8a92f4a39052b67b38af79c26117f3d53)
    (cherry picked from commit de7c02b5456d8b96bbdecfc7690411115091bc49)

--- compiler/GHC/StgToCmm/Config.hs
+++ compiler/GHC/StgToCmm/Config.hs
@@ -68,6 +68,8 @@ data StgToCmmConfig = StgToCmmConfig
   , stgToCmmAllowExtendedAddSubInstrs :: !Bool   -- ^ Allowed to generate AddWordC, SubWordC, Add2, etc.
   , stgToCmmAllowIntMul2Instr         :: !Bool   -- ^ Allowed to generate IntMul2 instruction
   , stgToCmmTickyAP                   :: !Bool   -- ^ Disable use of precomputed standard thunks.
+  , stgToCmmSaveFCallTargetToLocal    :: !Bool   -- ^ Save a foreign call target to a Cmm local, see
+                                                 -- Note [Saving foreign call target to local] for details
   ------------------------------ SIMD flags ------------------------------------
   -- Each of these flags checks vector compatibility with the backend requested
   -- during compilation. In essence, this means checking for @-fllvm@ which is
