32f12ceb6153c35c3afe756ab8186757ea46b0ca
Cheng Shao <terrorjack@type.dance>
2024-10-04 20:23:08 +0000

    compiler: fix checkNonStdWay for targets that require dynamic libraries
    This commit fixes checkNonStdWay to ensure that for targets whose RTS
    linker can only load dynamic code, the dynamic way of object is
    selected.
    
    (cherry picked from commit 94ef949ef8b52cebaf8d4a81d7a169e100da2a73)

--- compiler/GHC/Linker/Deps.hs
+++ compiler/GHC/Linker/Deps.hs
@@ -60,6 +60,7 @@ import System.Directory
 
 data LinkDepsOpts = LinkDepsOpts
   { ldObjSuffix   :: !String                        -- ^ Suffix of .o files
+  , ldForceDyn    :: !Bool                          -- ^ Always use .dyn_o?
   , ldOneShotMode :: !Bool                          -- ^ Is the driver in one-shot mode?
   , ldModuleGraph :: !ModuleGraph                   -- ^ Module graph
   , ldUnitEnv     :: !UnitEnv                       -- ^ Unit environment
@@ -349,6 +350,16 @@ throwProgramError opts doc = throwGhcExceptionIO (ProgramError (renderWithContex
 
 checkNonStdWay :: LinkDepsOpts -> Interp -> SrcSpan -> IO (Maybe FilePath)
 checkNonStdWay _opts interp _srcspan
+  -- On some targets (e.g. wasm) the RTS linker only supports loading
+  -- dynamic code, in which case we need to ensure the .dyn_o object
+  -- is picked (instead of .o which is also present because of
+  -- -dynamic-too)
+  | ldForceDyn _opts = do
+      let target_ways = fullWays $ ldWays _opts
+      pure $ if target_ways `hasWay` WayDyn
+        then Nothing
+        else Just $ waysTag (WayDyn `addWay` target_ways) ++ "_o"
+
   | ExternalInterp {} <- interpInstance interp = return Nothing
     -- with -fexternal-interpreter we load the .o files, whatever way
     -- they were built.  If they were built for a non-std way, then
