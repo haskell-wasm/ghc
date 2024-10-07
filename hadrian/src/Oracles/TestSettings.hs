6596dc02e474ddf41c44fbf282898ac576d144ed
Cheng Shao <terrorjack@type.dance>
2024-10-07 16:30:59 +0000

    hadrian/testsuite: implement config.cross logic
    This commit implements the config.cross field in the testsuite driver.
    It comes from the "cross compiling" ghc info field for both
    in-tree/out-of-tree GHC, and is an accurate predicate of whether we're
    cross-compiling or not (compared to the precense of target emulator),
    and is useful to implement predicates to assert the precense of
    internal interpreter (only available on non-cross GHC) for tests that
    do require it (e.g. plugins).
    
    (cherry picked from commit 2956a3f7ecd58a6fda81447100404941c0ed837d)

--- hadrian/src/Oracles/TestSettings.hs
+++ hadrian/src/Oracles/TestSettings.hs
@@ -30,6 +30,7 @@ data TestSetting = TestHostOS
                  | TestGhcDebugAssertions
                  | TestGhcWithNativeCodeGen
                  | TestGhcWithInterpreter
+                 | TestGhcCrossCompiling
                  | TestGhcWithRtsLinker
                  | TestGhcUnregisterised
                  | TestGhcTablesNextToCode
@@ -61,6 +62,7 @@ testSetting key = do
         TestGhcDebugAssertions    -> "GhcDebugAssertions"
         TestGhcWithNativeCodeGen  -> "GhcWithNativeCodeGen"
         TestGhcWithInterpreter    -> "GhcWithInterpreter"
+        TestGhcCrossCompiling     -> "CrossCompiling"
         TestGhcWithRtsLinker      -> "GhcWithRtsLinker"
         TestGhcUnregisterised     -> "GhcUnregisterised"
         TestGhcTablesNextToCode   -> "GhcTablesNextToCode"
@@ -124,4 +126,3 @@ stageOfTestCompiler "stage1" = Just stage0InTree
 stageOfTestCompiler "stage2" = Just Stage1
 stageOfTestCompiler "stage3" = Just Stage2
 stageOfTestCompiler _ = Nothing
-
