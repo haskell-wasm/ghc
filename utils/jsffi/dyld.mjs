1540dffc448885782600a95fdfc3ef93ec70baf8
Cheng Shao <terrorjack@type.dance>
2025-03-05 23:59:18 +0000

    wasm: do not use wasm type reflection in dyld
    The wasm dynamic linker used to depend on v8's experimental wasm type
    reflection support to generate stub functions when treating GOT.func
    items that aren't exported by any loaded library yet. However, as we
    work towards wasm ghci browser mode (#25399), we need to ensure the
    wasm dyld logic is portable across browsers. So this commit removes
    the usage of wasm type reflection in wasm dyld, and it shall only be
    added many months later when this feature is widely available in
    browsers.
    
    (cherry picked from commit cca68421831d0b5aadb82a649921188e343094e0)

--- utils/jsffi/dyld.mjs
+++ utils/jsffi/dyld.mjs
@@ -1182,30 +1182,15 @@ class DyLD {
             continue;
           }
 
-          // For lazy GOT.func entries we can do better than poison:
-          // insert a stub in the table, so we at least get an error
-          // message that includes the missing function's name, not a
-          // mysterious table trap. The function type is Cmm function
-          // type as a best effort guess, if there's a type mismatch
-          // then call_indirect would trap.
-          //
-          // Also set a __poison field since we can't compare value
-          // against DyLD.#poison.
+          // Can't find this function, so poison it like GOT.mem.
+          // TODO: when wasm type reflection is widely available in
+          // browsers, use the WebAssembly.Function constructor to
+          // dynamically create a stub function that does better error
+          // reporting
           this.#gotFunc[name] = new WebAssembly.Global(
             { value: "i32", mutable: true },
-            this.#table.grow(
-              1,
-              new WebAssembly.Function(
-                { parameters: [], results: ["i32"] },
-                () => {
-                  throw new WebAssembly.RuntimeError(
-                    `non-existent function ${name}`
-                  );
-                }
-              )
-            )
+            DyLD.#poison
           );
-          this.#gotFunc[name].__poison = true;
           continue;
         }
 
@@ -1291,7 +1276,7 @@ class DyLD {
     if (this.#gotMem[sym] && this.#gotMem[sym].value !== DyLD.#poison) {
       return this.#gotMem[sym].value;
     }
-    if (this.#gotFunc[sym] && !this.#gotFunc[sym].__poison) {
+    if (this.#gotFunc[sym] && this.#gotFunc[sym].value !== DyLD.#poison) {
       return this.#gotFunc[sym].value;
     }
     // Not in GOT.func yet, create the entry on demand
