497b25c1c03389f25b9e80e14cf0f335934ba839
Cheng Shao <terrorjack@type.dance>
2025-03-10 19:58:23 +0000

    ghci: make the Pipe type opaque
    This commit makes the Pipe type in ghci opaque, and introduce the
    mkPipeFromHandles constructor for creating a Pipe from a pair of
    Handles. Pipe is now just a pair of receiver/sender continuations
    under the hood. This allows a Pipe to be potentially backed by other
    IPC mechanisms (e.g. WebSockets) which is essential for wasm ghci
    browser mode.
    
    (cherry picked from commit 7d18c19b4c6a55d1fad3ca65c5a0754cf865bc26)
    (cherry picked from commit 4148bc13cc02986428ad716635ed8600471b4d50)

--- compiler/GHC/Runtime/Interpreter.hs
+++ compiler/GHC/Runtime/Interpreter.hs
@@ -108,7 +108,6 @@ import Control.Monad.Catch as MC (mask)
 import Data.Binary
 import Data.ByteString (ByteString)
 import Data.Array ((!))
-import Data.IORef
 import Foreign hiding (void)
 import qualified GHC.Exts.Heap as Heap
 import GHC.Stack.CCS (CostCentre,CostCentreStack)
@@ -577,12 +576,11 @@ spawnIServ conf = do
   (ph, rh, wh) <- runWithPipes createProc (iservConfProgram conf)
                                           []
                                           (iservConfOpts    conf)
-  lo_ref <- newIORef Nothing
+  interpPipe <- mkPipeFromHandles rh wh
   lock <- newMVar ()
-  let pipe = Pipe { pipeRead = rh, pipeWrite = wh, pipeLeftovers = lo_ref }
   let process = InterpProcess
                   { interpHandle = ph
-                  , interpPipe   = pipe
+                  , interpPipe
                   , interpLock   = lock
                   }
 
