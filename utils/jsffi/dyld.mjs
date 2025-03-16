98f2e16c92a2327b026848f78c5e2e5441324155
Cheng Shao <terrorjack@type.dance>
2025-03-16 19:26:20 +0000

    ghci: fix ^C handling for wasm iserv
    This commit fixes ^C handling for wasm iserv. Previously we didn't
    handle it at all, so ^C would kill the node process and host ghc would
    then crash as well. But native ghc with external interpreter can
    handle ^C just fine and wasm should be no different. Hence the fix:
    wasm iserv exports its signal handler as a js callback to be handled
    by the dyld script. Also see added note for details.
    
    (cherry picked from commit fa2fbd2b24c3e220700c7734a5829e28fe45d22a)

--- utils/jsffi/dyld.mjs
+++ utils/jsffi/dyld.mjs
@@ -748,6 +748,11 @@ if (isMain()) {
   const reader = in_stream.getReader();
   const writer = out_stream.getWriter();
 
+  const cb_sig = (cb) => {
+    process.on("SIGINT", cb);
+    process.on("SIGQUIT", cb);
+  };
+
   const cb_recv = async () => {
     const { done, value } = await reader.read();
     if (done) {
@@ -760,5 +765,5 @@ if (isMain()) {
   };
 
   dyld.exportFuncs.__ghc_wasm_jsffi_init();
-  await dyld.exportFuncs.defaultServer(cb_recv, cb_send);
+  await dyld.exportFuncs.defaultServer(cb_sig, cb_recv, cb_send);
 }
