fdf12abf897858319990808936a8ea74026a5b38
Cheng Shao <terrorjack@type.dance>
2025-10-11 21:04:34 +0200

    compiler: implement fast get/put for Word16/Word32/Word64
    Previously, `GHC.Utils.Binary` contains `get`/`put` functions for
    `Word16`/`Word32`/`Word64` which always loads and stores them as
    big-endian words at a potentially unaligned address. The previous
    implementation is based on loads/stores of individual bytes and
    concatenating bytes with bitwise operations, which currently cannot be
    fused to a single load/store operation by GHC.
    
    This patch implements fast `get`/`put` functions for
    `Word16`/`Word32`/`Word64` based on a single memory load/store, with
    an additional `byteSwap` operation on little-endian hosts. It is based
    on unaligned load/store primops added since GHC 9.10, and we already
    require booting with at least 9.10, so it's about time to switch to
    this faster path.
    
    (cherry picked from commit 66a0c4f7e9dc52ce076249582fcd1abc0a628813)
    (cherry picked from commit 4fe3c7b21a60b26a10c5f2091a1407858d0359ab)
    (cherry picked from commit d7238882f30b898988a68e0513c4a1adfdb710c1)

--- compiler/GHC/Utils/Binary.hs
+++ compiler/GHC/Utils/Binary.hs
@@ -1,6 +1,7 @@
 
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE UnboxedTuples #-}
 
 {-# OPTIONS_GHC -O2 -funbox-strict-fields #-}
@@ -110,14 +111,15 @@ import Data.Time
 import Data.List (unfoldr)
 import Control.Monad            ( when, (<$!>), unless, forM_, void )
 import System.IO as IO
-import System.IO.Unsafe         ( unsafeInterleaveIO )
 import System.IO.Error          ( mkIOError, eofErrorType )
 import GHC.Real                 ( Ratio(..) )
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
-#if MIN_VERSION_base(4,15,0)
+import GHC.ByteOrder
 import GHC.ForeignPtr           ( unsafeWithForeignPtr )
-#endif
+import GHC.Exts
+import GHC.IO
+import GHC.Word
 
 type BinArray = ForeignPtr Word8
 
@@ -395,71 +397,52 @@ getWord8 :: BinHandle -> IO Word8
 getWord8 h = getPrim h 1 peek
 
 putWord16 :: BinHandle -> Word16 -> IO ()
-putWord16 h w = putPrim h 2 (\op -> do
-  pokeElemOff op 0 (fromIntegral (w `shiftR` 8))
-  pokeElemOff op 1 (fromIntegral (w .&. 0xFF))
-  )
+putWord16 h w = putPrim h 2 $ \(Ptr p#) ->
+  IO $ \s -> (# writeWord8OffAddrAsWord16# p# 0# x# s, () #)
+  where
+    !(W16# x#) = case targetByteOrder of
+      BigEndian -> w
+      LittleEndian -> byteSwap16 w
 
 getWord16 :: BinHandle -> IO Word16
-getWord16 h = getPrim h 2 (\op -> do
-  w0 <- fromIntegral <$> peekElemOff op 0
-  w1 <- fromIntegral <$> peekElemOff op 1
-  return $! w0 `shiftL` 8 .|. w1
-  )
+getWord16 h = getPrim h 2 $ \(Ptr p#) ->
+  IO $ \s -> case readWord8OffAddrAsWord16# p# 0# s of
+    (# s', w16# #) -> case targetByteOrder of
+      BigEndian -> (# s', W16# w16# #)
+      LittleEndian -> case byteSwap16 $ W16# w16# of
+        !w16 -> (# s', w16 #)
 
 putWord32 :: BinHandle -> Word32 -> IO ()
-putWord32 h w = putPrim h 4 (\op -> do
-  pokeElemOff op 0 (fromIntegral (w `shiftR` 24))
-  pokeElemOff op 1 (fromIntegral ((w `shiftR` 16) .&. 0xFF))
-  pokeElemOff op 2 (fromIntegral ((w `shiftR` 8) .&. 0xFF))
-  pokeElemOff op 3 (fromIntegral (w .&. 0xFF))
-  )
+putWord32 h w = putPrim h 4 $ \(Ptr p#) ->
+  IO $ \s -> (# writeWord8OffAddrAsWord32# p# 0# x# s, () #)
+  where
+    !(W32# x#) = case targetByteOrder of
+      BigEndian -> w
+      LittleEndian -> byteSwap32 w
 
 getWord32 :: BinHandle -> IO Word32
-getWord32 h = getPrim h 4 (\op -> do
-  w0 <- fromIntegral <$> peekElemOff op 0
-  w1 <- fromIntegral <$> peekElemOff op 1
-  w2 <- fromIntegral <$> peekElemOff op 2
-  w3 <- fromIntegral <$> peekElemOff op 3
-
-  return $! (w0 `shiftL` 24) .|.
-            (w1 `shiftL` 16) .|.
-            (w2 `shiftL` 8)  .|.
-            w3
-  )
+getWord32 h = getPrim h 4 $ \(Ptr p#) ->
+  IO $ \s -> case readWord8OffAddrAsWord32# p# 0# s of
+    (# s', w32# #) -> case targetByteOrder of
+      BigEndian -> (# s', W32# w32# #)
+      LittleEndian -> case byteSwap32 $ W32# w32# of
+        !w32 -> (# s', w32 #)
 
 putWord64 :: BinHandle -> Word64 -> IO ()
-putWord64 h w = putPrim h 8 (\op -> do
-  pokeElemOff op 0 (fromIntegral (w `shiftR` 56))
-  pokeElemOff op 1 (fromIntegral ((w `shiftR` 48) .&. 0xFF))
-  pokeElemOff op 2 (fromIntegral ((w `shiftR` 40) .&. 0xFF))
-  pokeElemOff op 3 (fromIntegral ((w `shiftR` 32) .&. 0xFF))
-  pokeElemOff op 4 (fromIntegral ((w `shiftR` 24) .&. 0xFF))
-  pokeElemOff op 5 (fromIntegral ((w `shiftR` 16) .&. 0xFF))
-  pokeElemOff op 6 (fromIntegral ((w `shiftR` 8) .&. 0xFF))
-  pokeElemOff op 7 (fromIntegral (w .&. 0xFF))
-  )
+putWord64 h w = putPrim h 8 $ \(Ptr p#) ->
+  IO $ \s -> (# writeWord8OffAddrAsWord64# p# 0# x# s, () #)
+  where
+    !(W64# x#) = case targetByteOrder of
+      BigEndian -> w
+      LittleEndian -> byteSwap64 w
 
 getWord64 :: BinHandle -> IO Word64
-getWord64 h = getPrim h 8 (\op -> do
-  w0 <- fromIntegral <$> peekElemOff op 0
-  w1 <- fromIntegral <$> peekElemOff op 1
-  w2 <- fromIntegral <$> peekElemOff op 2
-  w3 <- fromIntegral <$> peekElemOff op 3
-  w4 <- fromIntegral <$> peekElemOff op 4
-  w5 <- fromIntegral <$> peekElemOff op 5
-  w6 <- fromIntegral <$> peekElemOff op 6
-  w7 <- fromIntegral <$> peekElemOff op 7
-
-  return $! (w0 `shiftL` 56) .|.
-            (w1 `shiftL` 48) .|.
-            (w2 `shiftL` 40) .|.
-            (w3 `shiftL` 32) .|.
-            (w4 `shiftL` 24) .|.
-            (w5 `shiftL` 16) .|.
-            (w6 `shiftL` 8)  .|.
-            w7
-  )
+getWord64 h = getPrim h 8 $ \(Ptr p#) ->
+  IO $ \s -> case readWord8OffAddrAsWord64# p# 0# s of
+    (# s', w64# #) -> case targetByteOrder of
+      BigEndian -> (# s', W64# w64# #)
+      LittleEndian -> case byteSwap64 $ W64# w64# of
+        !w64 -> (# s', w64 #)
 
 putByte :: BinHandle -> Word8 -> IO ()
 putByte bh !w = putWord8 bh w
