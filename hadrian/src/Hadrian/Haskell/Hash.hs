e8a804cd056cc1dad78701a860d530647100f836
Cheng Shao <terrorjack@type.dance>
2025-12-26 15:30:19 +0100

    hadrian: drop deprecated pkgHashSplitObjs code path
    This patch drops deprecated `pkgHashSplitObjs` code path from hadrian,
    since GHC itself has removed split objs support many versions ago and
    this code path is unused.
    
    (cherry picked from commit ab0a5594e1f6c785e711d38a41996abfb107695a)
    (cherry picked from commit 8afa3fa50e4883672cddf138b9d6e2e3ffc0bf0d)

--- hadrian/src/Hadrian/Haskell/Hash.hs
+++ hadrian/src/Hadrian/Haskell/Hash.hs
@@ -86,7 +86,6 @@ data PackageHashConfigInputs = PackageHashConfigInputs {
        pkgHashGHCiLib             :: Bool,
        pkgHashProfLib             :: Bool,
        pkgHashProfExe             :: Bool,
-       pkgHashSplitObjs           :: Bool,
        pkgHashSplitSections       :: Bool,
        pkgHashStripLibs           :: Bool,
        pkgHashStripExes           :: Bool,
@@ -143,7 +142,6 @@ pkgHashOracle = void $ addOracleCache $ \(PkgHashKey (stag, pkg)) -> do
       pkgHashGHCiLib = False
       pkgHashProfLib = profiling `Set.member` libWays
       pkgHashProfExe = pkg == ghc && ghcProfiled flav stag
-      pkgHashSplitObjs = False -- Deprecated
       pkgHashSplitSections = ghcSplitSections flav
       pkgHashStripExes = False
       pkgHashStripLibs = False
@@ -243,7 +241,6 @@ renderPackageHashInputs PackageHashInputs{
       , opt   "ghci-lib"    False show pkgHashGHCiLib
       , opt   "prof-lib"    False show pkgHashProfLib
       , opt   "prof-exe"    False show pkgHashProfExe
-      , opt   "split-objs"   False show pkgHashSplitObjs
       , opt   "split-sections" False show pkgHashSplitSections
       , opt   "stripped-lib" False show pkgHashStripLibs
       , opt   "stripped-exe" True  show pkgHashStripExes
