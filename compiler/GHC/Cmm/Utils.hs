691997c5ea903c4eeb7b627d603fb890d1f84cee
Cheng Shao <terrorjack@type.dance>
2026-01-12 16:32:28 +0100

    compiler: add myCapabilityExpr to GHC.Cmm.Utils
    This commit adds `myCapabilityExpr` to `GHC.Cmm.Utils` which is
    computed from `BaseReg`. It's convenient for codegen logic where one
    needs to pass the current Capability's pointer.
    
    (cherry picked from commit a46a1bb194582d971836bb2191c9c08f2c6bfcaa)
    (cherry picked from commit 7e09cdc4cd8e9445dd969e0457ef2a003ddcad6b)

--- compiler/GHC/Cmm/Utils.hs
+++ compiler/GHC/Cmm/Utils.hs
@@ -43,6 +43,7 @@ module GHC.Cmm.Utils(
 
         baseExpr, spExpr, hpExpr, spLimExpr, hpLimExpr,
         currentTSOExpr, currentNurseryExpr, cccsExpr,
+        myCapabilityExpr,
 
         -- Tagging
         cmmTagMask, cmmPointerMask, cmmUntag, cmmIsTagged, cmmIsNotTagged,
@@ -592,7 +593,7 @@ blockTicks b = reverse $ foldBlockNodesF goStmt b []
 -- Access to common global registers
 
 baseExpr, spExpr, hpExpr, currentTSOExpr, currentNurseryExpr,
-  spLimExpr, hpLimExpr, cccsExpr :: Platform -> CmmExpr
+  spLimExpr, hpLimExpr, cccsExpr, myCapabilityExpr :: Platform -> CmmExpr
 baseExpr           p = CmmReg $ baseReg           p
 spExpr             p = CmmReg $ spReg             p
 spLimExpr          p = CmmReg $ spLimReg          p
@@ -601,3 +602,5 @@ hpLimExpr          p = CmmReg $ hpLimReg          p
 currentTSOExpr     p = CmmReg $ currentTSOReg     p
 currentNurseryExpr p = CmmReg $ currentNurseryReg p
 cccsExpr           p = CmmReg $ cccsReg           p
+myCapabilityExpr   p =
+  cmmRegOff (baseReg p) $ negate $ pc_OFFSET_Capability_r $ platformConstants p
