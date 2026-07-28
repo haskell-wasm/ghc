4aecd904d6880236f6dc183b23acbe6d66006a5c
Zubin Duggal <zubin.duggal@gmail.com>
2026-07-28 15:15:12 +0530

    hadrian: add text and parsec to stage0Packages
    text depends on template-haskell-lift, and template-haskell-lift needs to be
    built in tree rather than using the boot package, so `text` and `parsec` (which
    depends on `text`) must also be built in tree rather than copied from the boot
    pkgdb.
    
    Also need to disable -simdutf for text when building the stage0 version, wasm backend
    doesn't like -simdutf.
    
    (cherry picked from commit b0fc1bd4a955193f44fcce0cd43b18ff33a789fc)

--- hadrian/src/Settings/Packages.hs
+++ hadrian/src/Settings/Packages.hs
@@ -187,7 +187,7 @@ packageArgs = do
 
         ---------------------------------- text --------------------------------
         , package text ?
-            ifM (textWithSIMDUTF <$> expr flavour)
+            ifM (notStage0 &&^ (textWithSIMDUTF <$> expr flavour))
               (builder (Cabal Flags) ? arg "+simdutf")
               (builder (Cabal Flags) ? arg "-simdutf")
 
